157bdd626a2424849858f829d9ebcb26f3c739cb
Alejandro Rosas <alejandrorosasdev@gmail.com>
2020-01-29 16:49:50 +0100

    Lint issue with annotation class

--- app/src/main/java/dev/alejandrorosas/apptemplate/MainActivity.kt
+++ app/src/main/java/dev/alejandrorosas/apptemplate/MainActivity.kt
@@ -1,5 +1,16 @@
 package dev.alejandrorosas.apptemplate
 
+import androidx.annotation.IntDef
 import dev.alejandrorosas.core.BaseActivity
 
-class MainActivity : BaseActivity(R.layout.activity_main)
+const val TAB_ONE = 0
+const val TAB_TWO = 1
+
+@IntDef(TAB_ONE, TAB_TWO)
+@Retention(AnnotationRetention.SOURCE)
+annotation class TAB
+
+class MainActivity : BaseActivity(R.layout.activity_main) {
+    @TAB
+    fun getTab(): Int = TAB_TWO
+}
